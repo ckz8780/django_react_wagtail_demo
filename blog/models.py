bfdf5e35ac35dd2189d524fad5dcdce73bc26f85
Christopher Zielinski <ckz8780@gmail.com>
2019-03-25 22:08:14 -0400

    updated models

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,9 @@ from modelcluster.contrib.taggit import ClusterTaggableManager
 from taggit.models import TaggedItemBase
 
 from wagtail.core.models import Page, Orderable
-from wagtail.core.fields import RichTextField
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
 from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
